fcfc06396409be01f51b0125f757fc19b1d2e0de
vtagithub <124488381+vtagithub@users.noreply.github.com>
2023-06-18 21:01:56 -0400

    Create 07_create_dba_accounts.sql

---
+++ 02_setup/07_create_dba_accounts.sql
@@ -0,0 +1,11 @@
+REM	#######################################################
+REM	07_create_dba_accounts.sql
+REM	#######################################################
+
+create user JOHN_ADMIN identified by "password123" profile vta_default password expire default tablespace NON_APP_DATA;
+grant DBA to JOHN_ADMIN;
+
+create user MIKE_ADMIN identified by "password123" profile vta_default password expire default tablespace NON_APP_DATA;
+grant DBA to JOHN_ADMIN;
+
+REM	#######################################################
