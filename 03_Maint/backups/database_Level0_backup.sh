ffb8976037b386cf080a05352d2dbb16e0fcba5e
vtagithub <124488381+vtagithub@users.noreply.github.com>
2023-06-18 21:36:36 -0400

    Update database_Level0_backup.sh

--- 03_Maint/backups/database_Level0_backup.sh
+++ 03_Maint/backups/database_Level0_backup.sh
@@ -2,3 +2,22 @@
 ##############################################################################################################
 # database_Level0_backup.sh
 ##############################################################################################################
+if [ -f $HOME/.profile ]
+then
+        . $HOME/.profile
+elif [ -f $HOME/.bash_profile ]
+then    
+        . $HOME/.bash_profile
+ else
+        ehco "A profile does not exit"
+ fi
+ 
+ if   [ -f /etc/oratab  ]
+ then
+      export  ORATAB='/etc/oratab'
+ elif [ -f /var/opt/oracle/oratab ]
+ then
+      export  ORATAB='/var/opt/oracle/oratab'
+ else
+      echo "An oratab file does not exists"
+fi
