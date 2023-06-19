cf401d3ad5682d0a9981e52a67354bcd59278da9
vtagithub <124488381+vtagithub@users.noreply.github.com>
2023-06-19 00:18:59 -0400

    Update cleanup_archives.sh

--- 03_Maint/log_management/cleanup_archives.sh
+++ 03_Maint/log_management/cleanup_archives.sh
@@ -1,1 +1,28 @@
-
+#!/bin/sh
+##############################################################################################################
+# cleanup_archives.sh
+##############################################################################################################
+if [ -f $HOME/.profile ]
+then
+        . $HOME/.profile
+elif [ -f $HOME/.bash_profile ]
+then    
+        . $HOME/.bash_profile
+ else
+        ehco "A profile does not exit"
+ fi
+ 
+ if   [ -f /etc/oratab  ]
+ then
+      export  ORATAB='/etc/oratab'
+ elif [ -f /var/opt/oracle/oratab ]
+ then
+      export  ORATAB='/var/opt/oracle/oratab'
+ else
+      echo "An oratab file does not exists"
+fi
+export DATE=`date +%Y%m%d-%H%M`
+cat $ORATAB | grep -v "^#  | grep -v "^$" | grep -v "*" | grep-v "+ASM" | awk -F: '{print $1} | whle read ORACLE_SID
+do
+    cd $ORACLE_BASE/admin/$ORACLE_SID/adump/archive
+    
