eed85c865a9a292ca4997877e14844943387b4d2
vtagithub <124488381+vtagithub@users.noreply.github.com>
2023-06-18 21:56:37 -0400

    Update database_Level0_backup.sh

--- 03_Maint/backups/database_Level0_backup.sh
+++ 03_Maint/backups/database_Level0_backup.sh
@@ -37,4 +37,36 @@ do
 
 rman target / log $HOME/log/"$ORACLE_SID"_LEVEL0Pbackup-"$RUNTIME".log<<EOF
          run
-         {
+         {      
+                allocate channel T1 type 'sbt_tape' PARMS 'ENV=(TDPO_OPTFILE=/opt/tivoli/tsm/client/oracle/bin64/tdpo.opt)' MAXPIECESIZE 10G;
+                allocate channel T2 type 'sbt_tape' PARMS 'ENV=(TDPO_OPTFILE=/opt/tivoli/tsm/client/oracle/bin64/tdpo.opt)' MAXPIECESIZE 10G;
+                backup incremental level 0 database plus archivelog;
+                release channel T1;
+                release channle T2;
+          }
+          quit
+          
+EOF
+
+RC=$?
+        if [ "$RC" = 0 ]; then
+                BSTATUS="SUCCESS"
+        else
+                BSTATUS="FAIL"    
+        fi
+                export BTIME=`date +%T`
+                echo "$HOSTNAME,$ORACL_SID,LEVEL0,$BSTATUS,$BDATE-$BTIME" >> $BACKUPHIST
+rman target / catalog rman/$RMANPASS@ALTDBARP log $HOME/log/resync_output.log<<EOF
+      resync catalog;
+      quit
+EOF
+
+      cat $HOME/log/resync_output.log >> $HOME/log/"ORACLE_SID"_LEVEL0_backup-"$RUNTIME".log
+      rm $HOEM/log/resync_output.log
+      echo "Database Backup Complete" >> $HOME/log/"$ORACLE_SID"_LEVEL0_backup-"$RUNTIME".log
+      echo "Return COde:" $RC >> $HOME/lgo/"ORACLE_SID"_LEVEL0_backup-"$RUNTIME".log
+      echo "---------------------end of file--------------------"  >> $HOME/log/"$ORACLE_SID"_LEVEL0_backup-"$RUNTIME".log
+      
+done
+exit $RC
+##############################################################################################################
