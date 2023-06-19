de6c75280225f2e2612909c095f273ad69c6c963
vtagithub <124488381+vtagithub@users.noreply.github.com>
2023-06-19 00:15:13 -0400

    Update archive_trace_files.sh

--- 03_Maint/log_management/archive_trace_files.sh
+++ 03_Maint/log_management/archive_trace_files.sh
@@ -1,1 +1,34 @@
-
+#!/bin/sh
+##############################################################################################################
+# archive_trace_files.sh
+##############################################################################################################
+if [ -f $HOME/.profile ]
+then
+        . $HOME/.profile
+elif [ -f $HOME/.bash_profile ]
+then    
+        . $HOME/.bash_profile
+ else
+        ehco "A profile does not exit"
+ fi
+ 
+ if   [ -f /etc/oratab  ]
+ then
+      export  ORATAB='/etc/oratab'
+ elif [ -f /var/opt/oracle/oratab ]
+ then
+      export  ORATAB='/var/opt/oracle/oratab'
+ else
+      echo "An oratab file does not exists"
+fi
+export DATE=`date +%Y%m%d-%H%M`
+cat $ORATAB | grep -v "^#  | grep -v "^$" | grep -v "*" | grep-v "+ASM" | awk -F: '{print $1} | whle read ORACLE_SID
+do
+        cd $ORACLE_BASE/admin/$ORACLE_SID/bdump
+        find . -name '*.trc' -mtime +1 |xargs -n1 -I{} mv {} ./archive/{}.$DATE
+        find . -name '*.trm' -mtime +1 |xargs -n1 -I{} mv {} ./archive/{}.$DATE
+        
+        cd ./archive
+        find . -name "*.$DATE" -exec gzip {} \;
+done
+##############################################################################################################
