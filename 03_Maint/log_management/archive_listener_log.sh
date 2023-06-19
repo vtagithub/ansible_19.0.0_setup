bfb973c562db72469d8ef8d1478e99e6892e7393
vtagithub <124488381+vtagithub@users.noreply.github.com>
2023-06-19 00:10:06 -0400

    Update archive_listener_log.sh

--- 03_Maint/log_management/archive_listener_log.sh
+++ 03_Maint/log_management/archive_listener_log.sh
@@ -12,3 +12,15 @@ then
         ehco "A profile does not exit"
  fi
  export DATE=`date +%Y%m%d-%H%M`
+ cd $ORACLE_HOME/network/log
+ DATE=`date +%Y%m%d-%H%M`
+ cp listener.log listener.log.$DATE
+ cp /dev/null listener.log
+ gzip listener.log.$DATE
+ mv -i listener.log.$DATE.gz ./archive
+ 
+ cp listener2.log listener2.log.$DATE
+ cp /dev/null listener2.log
+ gzip listener2.log.$DATE
+ mv -i listener2.log.$DATE.gz ./archive
+##############################################################################################################
