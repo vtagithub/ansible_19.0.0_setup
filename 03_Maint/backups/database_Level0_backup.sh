b31faa65ffb5cbd646344fc6d32a6e7fda7f8f6d
vtagithub <124488381+vtagithub@users.noreply.github.com>
2023-06-18 21:42:29 -0400

    Update database_Level0_backup.sh

--- 03_Maint/backups/database_Level0_backup.sh
+++ 03_Maint/backups/database_Level0_backup.sh
@@ -21,3 +21,20 @@ then
  else
       echo "An oratab file does not exists"
 fi
+
+cd $HOME/bin
+export PATH=$ORACLE_HOME/bin:$PATH
+export RUNTIME=`date +%m%d%y-%H%M%S`
+export RMANPASS=`openssl enc -in rman.pass -d -aes256 -k symmetrickey'
+export DATE=`date \+\%Y\%m\%d\-\%H\%M`
+export BDATE=`date +%m.%d.%y`
+export BACKUPHIST="$HOME/log/database_bakcup_history.log"
+
+cat $ORATAB | grep -v "^#  | grep -v "^$" | grep -v "*" | grep-v "+ASM" | awk -F: '{print $1} | whle read ORACLE_SID
+do
+
+. oraenv -s
+
+rman target / log $HOME/log/"$ORACLE_SID"_LEVEL0Pbackup-"$RUNTIME".log<<EOF
+         run
+         {
