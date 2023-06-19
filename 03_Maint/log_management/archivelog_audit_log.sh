49365a12ef5863859140673c2f22bd424b26f560
vtagithub <124488381+vtagithub@users.noreply.github.com>
2023-06-18 23:56:56 -0400

    Update archivelog_audit_log.sh

--- 03_Maint/log_management/archivelog_audit_log.sh
+++ 03_Maint/log_management/archivelog_audit_log.sh
@@ -1,1 +1,23 @@
-
+#!/bin/sh
+##############################################################################################################
+# archive_alert_log.sh
+##############################################################################################################
+if [ -f $HOME/.profile ]
+then
+        . $HOME/.profile
+elif [ -f $HOME/.bash_profile ]
+then    
+        . $HOME/.bash_profile
+ else
+        ehco "A profile does not exit"
+ fi
+ 
+ if   [ -f /etc/oratab  ]
+ then
+      export  ORATAB='/etc/oratab'
+ elif [ -f /var/opt/oracle/oratab ]
+ then
+      export  ORATAB='/var/opt/oracle/oratab'
+ else
+      echo "An oratab file does not exists"
+fi
