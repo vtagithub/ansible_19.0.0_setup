e66c8310e629cbccc94f76729aed0e7b9499a0de
vtagithub <124488381+vtagithub@users.noreply.github.com>
2023-06-21 01:38:40 -0400

    Update 08_create_security_Scan_Accounts.sql

--- 02_setup/08_create_security_Scan_Accounts.sql
+++ 02_setup/08_create_security_Scan_Accounts.sql
@@ -1,15 +1,15 @@
-REM	=======================================================
+REM=======================================================
 REM	08_create_security_Scan_Accounts.sql
-REM	=======================================================
+REM=======================================================
 
 --- Account for performing Database scans
 
- CREATE USER "SVC_VTASCAN" IDENTIFIED BU "password12345"
+ CREATE USER "SVC_VTASCAN" IDENTIFIED BY "password12345"
         DEFAULT TABLESPACE "NON_APP_DATA"
         TEMPORARY TABLESPACE "TEMP"
         PROFILE "SECURITY_SCAN_PROFILE"
  ;
  
  grant DBA to SVC_VTASCAN;
- REM	=======================================================
+ REM=======================================================
  
