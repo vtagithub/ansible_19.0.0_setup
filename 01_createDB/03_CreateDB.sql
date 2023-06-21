b067a4da70d90efd3c3e02b026cae53dab8f983d
vtagithub <124488381+vtagithub@users.noreply.github.com>
2023-06-21 01:16:08 -0400

    Update 03_CreateDB.sql

--- 01_createDB/03_CreateDB.sql
+++ 01_createDB/03_CreateDB.sql
@@ -6,13 +6,13 @@ set verify off
 ACCEPT sysPassword CHAR PROMPT 'Enter new password for SYS: ' HIDE
 ACCEPT systemPassword CHAR PROMPT 'Enter new password for SYSTEM: ' HIDE
 
-host /opt/oracel/prodcut/19.0.0/db/bin/srvctl add database -d $ORACLE_SID -0 /opt/oracle/prodcut/19.0.0/db -p +DATA/$ORACLE_SID/spfile$ORACLE_SID.ora -n $ORACLE_SID
+host /opt/oracle/prodcut/19.0.0/db/bin/srvctl add database -d $ORACLE_SID -0 /opt/oracle/prodcut/19.0.0/db -p +DATA/$ORACLE_SID/spfile$ORACLE_SID.ora -n $ORACLE_SID
 
-host /opt/oracel/prodcut/19.0.0/db/bin/srvctl disable database -d $ORACLE_SID
+host /opt/oracle/prodcut/19.0.0/db/bin/srvctl disable database -d $ORACLE_SID
 
-host /opt/oracel/prodcut/19.0.0/db/bin/orapwd file=host /opt/oracel/prodcut/19.0.0/db/dbs/orapw$ORACLE_SID force=y format=12
+host /opt/oracle/prodcut/19.0.0/db/bin/orapwd file=host /opt/oracle/prodcut/19.0.0/db/dbs/orapw$ORACLE_SID force=y format=12
 
-host /opt/oracel/prodcut/19.0.0/crs/bin/setasmgidwrap o=/opt/oracel/prodcut/19.0.0/db/bin/oracle
+host /opt/oracle/prodcut/19.0.0/crs/bin/setasmgidwrap o=/opt/oracle/prodcut/19.0.0/db/bin/oracle
 
 @$ORACLE_BASE/admin/$ORACLE_SID/scripts/04_CreateDBInstance.sql
 @$ORACLE_BASE/admin/$ORACLE_SID/scripts/05_CreateDBFiles.sql
