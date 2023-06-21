c031e18b918c0dec98011fdabc517ebbcebe92cd
vtagithub <124488381+vtagithub@users.noreply.github.com>
2023-06-21 01:43:55 -0400

    Update 09_create_emsmon_account.sql

--- 02_setup/09_create_emsmon_account.sql
+++ 02_setup/09_create_emsmon_account.sql
@@ -1,1 +1,16 @@
+REM=======================================================
+REM	09_create_emsmon_account.sql
+REM=======================================================
 
+--- 
+
+ CREATE USER "DB_MY_EMSMON" IDENTIFIED BY "password12345"
+        DEFAULT TABLESPACE "NON_APP_DATA"
+        TEMPORARY TABLESPACE "TEMP"
+        PROFILE "MONITORIN_PROFILE"
+ ;
+ 
+ grant VTA_CONNECT to DB_MY_EMSMON;
+CREATE PUBLIC SYNONYM DBA_TABLESPACE_USAGE_METRICS FOR DBA_TABLESPACE_USAGE_METRICS;
+
+REM=======================================================
